f66c9935d788501d594d1ec12351e92e9c733a15
Will Ricketts <will@willricketts.com>
2015-08-18 15:53:45 -0400

    adding services

---
+++ api/services/authFunctions.js
@@ -0,0 +1,12 @@
+module.exports = {
+  hashPassword: hashPassword,
+  verifyPassword: verifyPassword
+}
+
+function hashPassword(prehash, callback) {
+
+}
+
+function verifyPassword(hash, callback) {
+  
+}
---
+++ api/services/userFunctions.js
@@ -0,0 +1,12 @@
+module.exports = {
+  findUserById: findUserById,
+  findUsersByGrade: findUsersByGrade
+};
+
+function findUserById(id, res, err, callback) {
+
+}
+
+function fundUsersByGrade(grade, res, err, callback) {
+  
+}
