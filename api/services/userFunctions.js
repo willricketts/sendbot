2a2bc4fadbd9c6fcfa7de309a4d4c674a962ac13
Will Ricketts <will@willricketts.com>
2015-08-18 15:57:55 -0400

    fleshed out user functions

--- api/services/userFunctions.js
+++ api/services/userFunctions.js
@@ -4,9 +4,31 @@ module.exports = {
 };
 
 function findUserById(id, res, err, callback) {
-
+  User.findOne({ id: id }, function(err, user) {
+    if(err) {
+      res.serverError(err);
+    }
+    else {
+      callback(user);
+    }
+  });
 }
 
-function fundUsersByGrade(grade, res, err, callback) {
-  
+function fundUsersByGrade(type, grade, res, err, callback) {
+  if(type == 'boulder') {
+    User.find({ boulderGrade: grade }, function(err, users) {
+      if(err) {
+        res.serverError(err);
+      }
+      else {
+        callback(users)
+      }
+    });
+  }
+  else if(type == 'route') {
+
+  }
+  else {
+    callback([]);
+  }
 }
