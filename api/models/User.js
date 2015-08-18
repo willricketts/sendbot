1de18db8665e1714496d0c977f81aaa8c8ae9703
Will Ricketts <will@willricketts.com>
2015-08-18 15:42:18 -0400

    added user controller and model with attributes

---
+++ api/models/User.js
@@ -0,0 +1,41 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    email: {
+      type: 'string'
+      unique: true
+    },
+
+    password: {
+      type: 'string'
+    },
+
+    firstName: {
+      type: 'string'
+    },
+
+    lastName: {
+      type: 'string'
+    },
+
+    admin: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+
+    routeGrade: {
+      type: 'integer'
+    },
+
+    boulderGrade: {
+      type: 'integer'
+    }
+  }
+};
