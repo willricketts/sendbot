3deb0b7a9aee7ad17a27b8ac1f436756c1e8ad55
Will Ricketts <will@willricketts.com>
2015-08-19 12:43:25 -0400

    added password hashing

--- api/services/authFunctions.js
+++ api/services/authFunctions.js
@@ -1,12 +1,34 @@
+var bcrypt = require('bcrypt');
+
 module.exports = {
   hashPassword: hashPassword,
   verifyPassword: verifyPassword
 }
 
 function hashPassword(prehash, callback) {
-
+  bcrypt.genSalt(10, function(err, salt) {
+    if(err) {
+      callback(new Error(err));
+    }
+    else if(salt) {
+      bcrypt.hash(prehash, salt, function(err, hash) {
+        if(err) {
+          callback(new Error(err));
+        }
+        else if(hash) {
+          callback(hash);
+        }
+        else {
+          callback(new Error('Password hashing failed somewhere, somehow'));
+        }
+      });
+    }
+    else {
+      callback(new Error('Salt was unhandled'));
+    }
+  });
 }
 
 function verifyPassword(hash, callback) {
-  
+
 }
